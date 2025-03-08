fa4a6cd75a103a6d699f598f050f5e2a875a5f07
Adnan <artoox61@googlemail.com>
2025-03-08 15:25:15 +0100

    WIP on Model Construction

--- Ground_Order_Resolution_Completeness.thy
+++ Ground_Order_Resolution_Completeness.thy
@@ -41,7 +41,6 @@ qed
                              
 declare epsilon.simps[simp del]
 
-
 lemma (in ground_order_resolution_calculus) epsilon_eq_empty_or_singleton:
   "epsilon N C = {} \<or> (\<exists>A. epsilon N C = {A})"
 proof -
@@ -58,6 +57,7 @@ proof -
       \<not> R\<^sub>C \<TTurnstile> C)"
     using Uniq_antimono'
     by (smt (verit) Uniq_def Uniq_prodI case_prod_conv)
+
   show ?thesis
     unfolding epsilon.simps[of N C]
     using Collect_eq_if_Uniq[OF Uniq_epsilon]
@@ -65,7 +65,65 @@ proof -
         insertCI mem_Collect_eq)
 qed
 
-lemma (in ground_order_resolution_calculus
+definition (in ground_order_resolution_calculus) rewrite_sys where
+  "rewrite_sys N C \<equiv> (\<Union>D \<in> {D \<in> N. D \<prec>\<^sub>c C}. epsilon N D)"
+
+
+lemma (in ground_order_resolution_calculus) mem_epsilonE:
+  assumes rule_in: "A \<in> epsilon N C"
+  obtains C' where
+    "C \<in> N" and
+    "C = add_mset (Pos A) C'" and
+    "select C = {#}" and
+    "is_strictly_maximal (Pos A) C" and
+    "\<not> rewrite_sys N C \<TTurnstile> C"
+  using rule_in
+  unfolding epsilon.simps[of N C] mem_Collect_eq Let_def rewrite_sys_def
+  by (metis (no_types, lifting))
+
+lemma (in ground_order_resolution_calculus) pre_model_construction:
+  fixes
+    N :: "'f gterm clause set" and
+    C :: "'f gterm clause"
+  defines
+    "entails \<equiv> \<lambda>E C. E \<TTurnstile> C"
+  assumes "saturated N" and "{#} \<notin> N" and C_in: "C \<in> N"
+  shows
+    "epsilon N C = {} \<longleftrightarrow> entails (rewrite_sys N C) C"
+    "\<And>D. D \<in> N \<Longrightarrow> C \<prec>\<^sub>c D \<Longrightarrow> entails (rewrite_sys N D) C"
+    unfolding atomize_all atomize_conj atomize_imp
+    using clause.order.wfp C_in
+proof (induction C arbitrary: D rule: wfp_induct_rule)
+  case (less C)
+  note IH = less.IH
+
+  from \<open>{#} \<notin> N\<close> \<open>C \<in> N\<close> have "C \<noteq> {#}"
+    by metis
+
+  define I :: "'f gterm set" where
+    "I = rewrite_sys N C"
+
+  have i: "(epsilon N C = {}) \<longleftrightarrow> entails (rewrite_sys N C) C"
+  proof (rule iffI)
+    show "entails (rewrite_sys N C) C \<Longrightarrow> epsilon N C = {}"
+      unfolding entails_def rewrite_sys_def
+      by (metis (no_types) empty_iff equalityI mem_epsilonE rewrite_sys_def subsetI)
+  next
+
+lemma (in ground_order_resolution_calculus) model_construction:
+  fixes
+    N :: "'f gterm clause set" and
+    C :: "'f gterm clause"
+  assumes "saturated N" and "{#} \<notin> N" and C_in: "C \<in> N"
+  shows
+    "epsilon N C = {} \<longleftrightarrow> interp N C \<TTurnstile> C"
+    "(\<Union>D \<in> N. epsilon N D) \<TTurnstile> C"
+    "D \<in> N \<Longrightarrow> C \<prec>\<^sub>c D \<Longrightarrow> interp N D \<TTurnstile> C"
+  unfolding atomize_conj atomize_imp
+  using epsilon_eq_empty_or_singleton[of N C]
+proof (elim disjE exE)
+  
+
 
 end
 end
