b5b625dfbccd9a5be28c81a2fa431692be9e97a5
Adnan <artoox61@googlemail.com>
2025-03-31 17:19:22 +0200

    Changes to Root and added Lifting File

---
+++ Ground_Order_Resolution_Lifting.thy
@@ -0,0 +1,13 @@
+theory Ground_Order_Resolution_Lifting
+  imports
+    Ground_Order_Resolution_Completeness
+begin
+
+context ground_order_resolution_calculus
+begin
+
+lemma resolution_lifting:
+
+
+end
+end
